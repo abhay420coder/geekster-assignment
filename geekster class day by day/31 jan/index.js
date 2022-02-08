f8b0e12ebf295c01c5fcdf5dd6fbfb3f2b07603c
abhay420coder <96329957+abhay420coder@users.noreply.github.com>
2022-02-08 18:54:23 +0530

    Add files via upload

---
+++ geekster class day by day/31 jan/index.js
@@ -0,0 +1,35 @@
+var obj = [      { 
+                     "name": "Name 1", 
+                     "marks": 15
+                }, 
+
+                {  
+                    "name": "Name 2",  
+                    "marks": 17  
+                },
+              
+              { 
+                "name": "Name 3",
+                "marks": 19
+              },
+
+              {  
+                "name": "Name 4",
+                "marks": 21
+              },
+
+              {
+                "name": "Name 5",
+                "marks": 0
+              },
+     
+              {
+  
+                "name": "Name 6",
+                "marks": 27
+              }
+  
+            ]
+
+
+console.log(obj)
